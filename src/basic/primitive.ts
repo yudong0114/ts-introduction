eb0f8e4f935a9f6570ab54cb8052da34baa76577
yudong0114 <yukisaito114@gmail.com>
2021-05-20 00:56:16 +0900

    [update]基本の型定義とアノテーションの確認

--- src/basic/primitive.ts
+++ src/basic/primitive.ts
@@ -1,6 +1,12 @@
 // プリミティブ型のテスト
 export default function primitiveSample() {
-  // プリミティブ型：string(VSCode上でのESLintのエラーは型推論によるもの)
+  // 型定義をした場合、VSCode上でのESLintのエラーが表示される(型推論についてのエラー)
+  // コミットした場合、ESLintの影響で、
+  // ・「型推論により、明示的な形定義のコードの削除」
+  // ・「letで複数回使われていない場合constに置き換え」が行われる
+  // let name: string = 'テスト 名前' -> const name = 'テスト 名前'
+
+  // プリミティブ型：string
   const name = 'テスト 名前'
   // name = 123 -> numberなのでエラー
   console.log('primitive sample 1:', typeof name, name)
