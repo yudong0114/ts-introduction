17dc525861ee14b36e519915efd3b667416652b5
yudong0114 <yukisaito114@gmail.com>
2021-05-22 02:19:47 +0900

    [update]型エイリアス(type)でオブジェクトの型定義

--- src/index.ts
+++ src/index.ts
@@ -19,28 +19,33 @@
 // console.log('/*----------*/')
 // unknownSample()
 
-// 05. 関数のパラメーターと戻り値に型をつける
-import {
-  logMessage,
-  logMessage2,
-  logMessage3,
-  logMessage4,
-  alwaysThronError,
-  logMessage6,
-  logMessage7,
-} from './function/basic'
-import { isUserSignedIn, isUserSignedIn2, sumProductsPrice } from './function/parameters'
+// // 05. 関数のパラメーターと戻り値に型をつける
+// import {
+//   logMessage,
+//   logMessage2,
+//   logMessage3,
+//   logMessage4,
+//   alwaysThronError,
+//   logMessage6,
+//   logMessage7,
+// } from './function/basic'
+// import { isUserSignedIn, isUserSignedIn2, sumProductsPrice } from './function/parameters'
 
-logMessage('Hello Typescript!')
-logMessage2('Hello Typescript!')
-logMessage3('Hello Typescript!')
-logMessage4('Hello Typescript!')
-// alwaysThronError('Hello Typescript!')
-logMessage6('Hello Typescript!')
-logMessage7('Hello Typescript!')
-isUserSignedIn('ABC', 'yamada katsumi')
-isUserSignedIn('DEF')
-isUserSignedIn2('ABC')
-isUserSignedIn2('DEF')
-const sum = sumProductsPrice(100, 200, 300, 400, 500)
-console.log('Function parameters sample 5:', sum)
+// logMessage('Hello Typescript!')
+// logMessage2('Hello Typescript!')
+// logMessage3('Hello Typescript!')
+// logMessage4('Hello Typescript!')
+// // alwaysThronError('Hello Typescript!')
+// logMessage6('Hello Typescript!')
+// logMessage7('Hello Typescript!')
+// isUserSignedIn('ABC', 'yamada katsumi')
+// isUserSignedIn('DEF')
+// isUserSignedIn2('ABC')
+// isUserSignedIn2('DEF')
+// const sum = sumProductsPrice(100, 200, 300, 400, 500)
+// console.log('Function parameters sample 5:', sum)
+
+// 06. 型エイリアス(type)でオブジェクトの型定義
+import objectSample from './object/object'
+
+objectSample()
---
+++ src/object/object.ts
@@ -0,0 +1,45 @@
+// objectのテスト
+export default function objectSample() {
+  // object型で型定義(エラー：Don't use `object` as a type. The `object` type is currently hard to use)
+  // const a: object = {
+  //   name: 'Taro',
+  //   age: 25
+  // }
+  // a.name -> エラーになる
+
+  // オブジェクトリテラル記法で型定義
+  let country: {
+    language: string
+    name: string
+  } = {
+    language: 'Japanese',
+    name: 'Japan',
+  }
+
+  console.log('Object object sample 1', country)
+
+  country = {
+    language: 'English',
+    name: 'United States of America',
+  }
+
+  console.log('Object object sample 2', country)
+
+  // オプショナルとreadonly
+  const profile: {
+    age: number
+    lastname: string
+    readonly firstname: string
+    gender?: string
+  } = {
+    age: 25,
+    lastname: 'Yamada',
+    firstname: 'Katsumi',
+  }
+
+  profile.gender = 'male'
+  profile.lastname = 'Nagano'
+  // profile.firstname = 'Makoto' -> readonlyのためエラー
+
+  console.log('Object object sample 3', profile)
+}
